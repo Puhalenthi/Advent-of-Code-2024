32035e5d9a1d0001ab1790fc51f88742b3ba5ed3
unknown <puhalenthirv@gmail.com>
2024-12-08 11:46:09 -0500

    add day5

---
+++ day05/part1.py
@@ -0,0 +1,47 @@
+import math
+
+def processRules(rules):
+    dictionary = {}
+    for rule in rules:
+        k, v = rule.split('|')
+        if int(k) in dictionary.keys():
+            dictionary[int(k)].append(int(v))
+        else:
+            dictionary[int(k)] = [int(v)]
+    return dictionary
+
+def processUpdates(updates):
+    lst = []
+    for update in updates:
+        temp = []
+        for i in update.split(','):
+            temp += [int(i)]
+        lst.append(temp)
+    return lst
+
+def checkIfValid(rules, update):
+    for i in range(len(update)):
+        for j in range(i+1, len(update)):
+            if update[j] in rules.keys():
+                if update[i] in rules[update[j]]:
+                    return False
+    return True
+
+
+with open('day05/input.txt') as f:
+    data = f.read().strip().split('\n')
+    rules, updates = data[0:data.index('')], data[data.index('')+1:]
+
+    rules = processRules(rules)
+    updates = processUpdates(updates)
+
+    validUpdates = []
+    for update in updates:
+        if checkIfValid(rules, update):
+            validUpdates.append(update)
+    
+    total = 0
+    for i in validUpdates:
+        total += i[math.floor(len(i)/2)]
+    
+    print(total)
